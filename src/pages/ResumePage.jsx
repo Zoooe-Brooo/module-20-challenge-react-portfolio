774d9b463d528bff60eecff832cdfcac5099d202
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-11-22 00:57:42 +1030

    update skills

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -12,21 +12,22 @@ export default function ResumePage() {
             <li>HTML</li>
             <li>CSS</li>
             <li>JavaScript</li>
-            <li>jQuery</li>
             <li>responsive design</li>
-            <li>React</li>
             <li>Bootstrap</li>
+            <li>Handlebars</li>
+            <li>React</li>
           </ul>
         </div>
         <div>
           <h3>Back-end Proficiencies</h3>
           <ul className="proficiencies-list">
-            <li>APIs</li>
             <li>Node</li>
             <li>Express</li>
-            <li>MySQL, Sequelize</li>
-            <li>MongoDB, Mongoose</li>
-            <li>REST</li>
+            <li>REST APIs</li>
+            <li>OOP</li>
+            <li>ORM</li>
+            <li>MySQL</li>
+            <li>MongoDB</li>
             <li>GraphQL</li>
           </ul>
         </div>
