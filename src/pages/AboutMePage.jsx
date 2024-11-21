cc7329877df579ed827b37e9a650362018b0b380
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-11-22 01:08:06 +1030

    update about me

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -6,8 +6,10 @@ export default function AboutMePage() {
     <div>
       <h2>About Me</h2>
       <img src={profilePicture} id="profile-img" alt="profile picture of zoe" />
-      <p>I am passionate about being a new web developer in a near future!</p>
-      <p>I was born and grow up in China, and first came to Australia in 2016. My background are Bachelor of Law and Master of Social Work, despite irrelevant to IT Industry, I still believe there are some transferable abilities and skills that I can utilise to my new role.</p>  
+      <p>I have recently and successfully completed a six-month Full Stack Coding Bootcamp study, and I’m passionate about building a career in web development.</p>
+      <p>I was born and grew up in China, and first came to Australia in 2016. Previously I have studied Bachelor of Law & Master of Social Work. My current job position is a youth worker and I work with children and young people from traumatic backgrounds.</p>  
+      <p>During my time in the web development course, instructors and classmates noted my ability to work both individually and collaborativley across multiple projects. I further developed my problem solving skills when working on my own and I also can recognise when I need to seek support. My logical thinking skills highlight my proficiency in back end web development and adaptable nature for front end.</p>
+      <p>Additionally, I believe there are several transferable skills from my current position as a youth worker that I can utilise in a career in web development such as documentation skills, communication skills, building and maintaining strong relationship within the team as well as client.</p>
     </div>
   );
 }
