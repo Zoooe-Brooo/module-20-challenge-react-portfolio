37695df40b16a874fd6c791218ea4da135943e22
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-11-25 14:41:28 +1030

    update order of portfolio

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -10,45 +10,47 @@ import Taskable from '../assets/taskable-screenshot.png';
 
 const projects = [
     {
+        image: Taskable,
+        name: "Taskable - Group Project",
+        website: "https://taskable.onrender.com/",
+        github: "https://github.com/Zoooe-Brooo/taskable",
+        project_id: 1,
+    },
+    {
+        
+        image: BookHive,
+        name: "Book Hive - Group Project",
+        website: "https://book-hive-c90u.onrender.com/",
+        github: "https://github.com/Zoooe-Brooo/book-hive",
+        project_id: 2,
+    },
+    {
+       
+        image: StudentSearchEngine,
+        name: "Student Search Engine - Group Project",
+        website: "https://jovana667.github.io/student-search-engine/",
+        github: "https://github.com/Zoooe-Brooo/student-search-engine",
+        project_id: 3,
+    },
+    {
         image: PersonalBlog,
         name: "Personal Blog",
         website: "https://zoooe-brooo.github.io/module-4-challenge-personal-blog/",
         github: "https://github.com/Zoooe-Brooo/module-4-challenge-personal-blog",
-        project_id: 1,
+        project_id: 4,
     },
     {
         image: TaskBoard,
         name: "Task Board",
         website: "https://zoooe-brooo.github.io/module-5-challenge-task-board/",
         github: "https://github.com/Zoooe-Brooo/module-5-challenge-task-board",
-        project_id: 2,
+        project_id: 5,
     },
     {
         image: WeatherDashboard,
         name: "Weather Dashboard",
         website: "https://zoooe-brooo.github.io/module-6-challenge-weather-dashboard/",
         github: "https://github.com/Zoooe-Brooo/module-6-challenge-weather-dashboard",
-        project_id: 3,
-    },
-    {
-        image: Taskable,
-        name: "Taskable - Group Project",
-        website: "https://taskable.onrender.com/",
-        github: "https://github.com/Zoooe-Brooo/taskable",
-        project_id: 4,
-    },
-    {
-        image: BookHive,
-        name: "Book Hive - Group Project",
-        website: "https://book-hive-c90u.onrender.com/",
-        github: "https://github.com/Zoooe-Brooo/book-hive",
-        project_id: 5,
-    },
-    {
-        image: StudentSearchEngine,
-        name: "Student Search Engine - Group Project",
-        website: "https://jovana667.github.io/student-search-engine/",
-        github: "https://github.com/Zoooe-Brooo/student-search-engine",
         project_id: 6,
     },
     {
