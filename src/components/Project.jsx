54746fdb40c9f4c1276f6d0c95a7cedeaddc8b21
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-12-05 17:58:05 +1030

    update so for click project picture to go to the webpage

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ export default function Project({ image, name, website, github }) {
     return (
         <>
           <div className="project-container">
-            <img src={image} className="project-img" alt="Project Picture" />
+            <a href={website}><img src={image} className="project-img" alt="Project Picture" /></a>
             <a href={github} className="project-anchor github-anchor">➔GitHub Repo</a>
             <a href={website} className="project-anchor website-anchor">{name}</a>
           </div>
